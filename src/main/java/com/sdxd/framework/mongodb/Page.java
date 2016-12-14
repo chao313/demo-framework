4bcdbc9be5aff306398c6fa33bbc754a0a9c38ef
Wenjun Wang <wangwenjun@shengdaxd.com>
2016-12-14 09:52:10 +0800

    实现序列化接口

--- src/main/java/com/sdxd/framework/mongodb/Page.java
+++ src/main/java/com/sdxd/framework/mongodb/Page.java
@@ -1,11 +1,12 @@
 package com.sdxd.framework.mongodb;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class Page<T> {
+public class Page<T> implements Serializable{
 
 	private Long total;
 	private Integer pageNumber;
