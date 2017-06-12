92f58d45fcb3cbecced9ef71baac8e1a9efc154d
仇扬骏 <qiuyangjun@shengdaxd.com>
2017-06-12 19:18:02 +0800

    删除获取spring上下文锁

--- src/main/java/com/sdxd/framework/context/ContextUtils.java
+++ src/main/java/com/sdxd/framework/context/ContextUtils.java
@@ -8,42 +8,42 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ContextUtils implements ApplicationContextAware{
+public class ContextUtils implements ApplicationContextAware {
 
-	private static ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
 
-	private static final Logger logger = LoggerFactory.getLogger(ContextUtils.class);
+    private static final Logger logger = LoggerFactory.getLogger(ContextUtils.class);
 
-	public static ApplicationContext getApplicationContext() {
-		synchronized (ContextUtils.class) {
-			while (applicationContext == null) {
-				try {
-					ContextUtils.class.wait(60000);
-					if (applicationContext == null) {
-						logger.warn("Have been waiting for ApplicationContext to be set for 1 minute", new Exception());
-					}
-				} catch (InterruptedException ex) {
-					logger.debug("getApplicationContext, wait interrupted");
-				}
-			}
-			return applicationContext;
-		}
-	}
+    public static ApplicationContext getApplicationContext() {
+//		synchronized (ContextUtils.class) {
+//			while (applicationContext == null) {
+//				try {
+//					ContextUtils.class.wait(60000);
+//					if (applicationContext == null) {
+//						logger.warn("Have been waiting for ApplicationContext to be set for 1 minute", new Exception());
+//					}
+//				} catch (InterruptedException ex) {
+//					logger.debug("getApplicationContext, wait interrupted");
+//				}
+//			}
+        return applicationContext;
+//		}
+    }
 
-	public static Object getBean(Class<?> beanType) {
-		return getApplicationContext().getBean(beanType);
-	}
+    public static Object getBean(Class<?> beanType) {
+        return getApplicationContext().getBean(beanType);
+    }
 
-	public static Object getBean(String name) {
-		return getApplicationContext().getBean(name);
-	}
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
-		synchronized (ContextUtils.class) {
-			ContextUtils.applicationContext = applicationContext;
-			ContextUtils.class.notifyAll();
-		}
-	}
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        synchronized (ContextUtils.class) {
+            ContextUtils.applicationContext = applicationContext;
+            ContextUtils.class.notifyAll();
+        }
+    }
 
 }
